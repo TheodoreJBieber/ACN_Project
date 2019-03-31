9fe5bdcac2c1e84f53a29313a96b13dc5ffb9de0
Tom White <twhite@wpi.edu>
2019-03-31 00:00:15 -0400

    Switch out Scheduler for custom Thread
    Remove the sched class because it does not work with blocking `sniff`
    
    Add a custom Thread class that loops itself with the ability to send a
    stop signal
    
    Add a `last_packet` variable to get around not being able to peek the
    end of the queue
    
    Update .gitignore

--- capture_packets.py
+++ capture_packets.py
@@ -14,29 +14,90 @@ Authors:
 '''
 # pip installs: libpcap, scapy==2.4.0 !!! scapy version > 2.4.0 has unresolved issues on windows
 # also could do pip install -r requirements.txt
-import sched, time
+import time
+from threading import Event, Thread
 
 # also install npcap https://nmap.org/download.html (latest Npcap release self-install worked for me)
 from scapy.all import sniff, IP, TCP
 
-import flow
+from flow import Flow
 import burst
 import queue # for our packets or bursts
 
-reactor = sched.scheduler(time.time, time.sleep)
+# Used to signal to the analysis thread to stop
+shutdown_event = Event()
+
+# queue for storing our packets, Queue is FIFO, we can also do LifoQueue, PriorityQueue, or a SimpleQueue that is a less useful version of Queue
+packet_queue = queue.Queue(0) # 0 is the default, indicating that the maximum size is infinite
+last_packet = None
+
+class AnalysisThread(Thread):
+	"""
+	Custom Thread class to loop continuously in the background
+	and perform analysis if the burst threshold has been met
+	"""
+	def __init__(self, event):
+		Thread.__init__(self)
+		self.stop_flag = event
+
+	def run(self):
+		while not self.stop_flag.wait(.45):
+			print("Checking for burst...")
+			self.check_for_burst()
+
+	def check_for_burst(self):
+		"""
+		Used to check if analysis should start
+		"""
+		# Don't process if there is nothing to process
+		if packet_queue.qsize() > 0:
+			if time.time() - last_packet.time > 1:
+				self.analyze_burst(last_packet)
+
+	def analyze_burst(self, last_packet):
+		"""
+		Handles putting packets into flows, prints, and then archives the burst
+		stops processing when `last_packet` is reached
+		"""
+		current_pkt = packet_queue.get()
+		identified_flows = []
+
+		# Group the packets into flows
+		while True:
+			for data_flow in identified_flows:
+				if data_flow == current_pkt:
+					data_flow += current_pkt
+					break
+			# The else statement will trigger if the break never happens
+			else:
+				identified_flows.append(Flow(
+					current_pkt[IP].src,
+					current_pkt[IP].dst,
+					current_pkt[TCP].sport,
+					current_pkt[TCP].dport,
+				))
+
+			if current_pkt is not last_packet:
+				current_pkt = packet_queue.get()
+			# We've reached the end of the burst,
+			# stop dequing
+			else:
+				break
+
+		# Print out the statistics (formatted in the Flow)
+		for flow in identified_flows:
+			print(flow)
+
 
 def main():
 	FILTER = "ip" # TODO: gonna wanna change this
 
 	# TODO: thread this, and then run another thread to print flows
 	# Does the ^above^ comment make sense to do?
-	reactor.enter(.45, 1, check_for_burst)
-	reactor.run()
+	analysis = AnalysisThread(shutdown_event)
+	analysis.start()
 	sniff(filter=FILTER, prn=handle_sniffed, count=0) # count = 0 means run indefinitely
-
-# queue for storing our packets, Queue is FIFO, we can also do LifoQueue, PriorityQueue, or a SimpleQueue that is a less useful version of Queue
-packet_queue = queue.Queue(0) # 0 is the default, indicating that the maximum size is infinite
-mylist = []
+	shutdown_event.set()
 
 
 ''' handle_sniffed:
@@ -51,56 +112,13 @@ def handle_sniffed(packet):
 	# subscripting packets is to access specific layers, like TCP or IP. These must be imported from scapy.all
 	# to get specific elements reference https://blogs.sans.org/pen-testing/files/2016/04/ScapyCheatSheet_v0.2.pdf
 	# put_in_flow(packet)
+	global last_packet
+	print("packet sniffed")
 	if packet.haslayer(TCP) and packet.haslayer(IP): # so this line we will probably need to remove at some point, but we also need the IP and TCP layers in order to get some information with scapy
+		last_packet = packet
 		packet_queue.put(packet)
 		
 		
-
-def check_for_burst():
-	"""
-	Used by the scheduler `reactor` to see if analysis should start
-	Will also restart the scheduler in order to keep a constant loop
-	"""
-	reactor.enter(.45, 1, check_for_burst)
-	reactor.run()
-	# Don't process if there is nothing to process
-	if len(packet_queue) > 0:
-		if time.time() - packet_queue[-1].time > 1:
-			analyze_burst(packet_queue[-1])
-
-def analyze_burst(last_packet):
-	"""
-	Handles putting packets into flows, prints, and then archives the burst
-	stops processing when `last_packet` is reached
-	"""
-	current_pkt = packet_queue.get()
-	identified_flows = []
-
-	# Group the packets into flows
-	while True:
-		for flow in identified_flows:
-			if flow == current_pkt:
-				flow += current_pkt
-				break
-		# The else statement will trigger if the break never happens
-		else:
-			identified_flows.append(flow.Flow(
-				current_pkt.source_ip,
-				current_pkt.dest_ip,
-				current_pkt.source_port,
-				current_pkt.dest_port,
-			))
-
-		if current_pkt is not last_packet:
-			current_pkt = packet_queue.get()
-		# We've reached the end of the burst,
-		# stop dequing
-		else:
-			break
-
-	# Print out the statistics (formatted in the Flow)
-	for flow in identified_flows:
-		print(flow)
 
 
 # # make sure to log flows on exit (no longer think this is necessary, but will leave for now)
@@ -112,4 +130,5 @@ def analyze_burst(last_packet):
 
 # run main
 if __name__ == '__main__':
+	print(packet_queue.qsize())
 	main()
